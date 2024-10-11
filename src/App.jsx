e010587b5f29b5a3dcd22010fbd45cca815b5495
akmtasdikulislam <akmtasdikulislam@gmail.com>
2024-10-11 18:49:39 +0600

    added '/appointment' route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom"; // Used for defining routes an
 import "./App.css"; // Imports global styles for the app
 
 /** Page components */
+import Appointment from "./pages/Appointment/Appointment"; // Imports the Appointment component from the specified path to be used in the app's routing or rendering
 import Home from "./pages/Home/Home"; // Imports the Home component to be rendered for the root route
 
 function App() {
@@ -12,8 +13,10 @@ function App() {
     <>
       <Routes>
         {/* Define the route for the home page */}
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         {/* Render the Home component when the path is "/" */}
+        <Route path="/appointment" element={<Appointment />} />
+        {/* Render the Appointment component when the path is "/appointment" */}
       </Routes>
       {/* Routes component wraps all Route components for defining app navigation */}
     </>
